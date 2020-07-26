b348a7a2b6e975c005aac21858d02c45db093cdf
prashant sikarwar <prashant97sikarwar@gmail.com>
2020-07-26 21:50:54 +0530

    daily coding practice

---
+++ Daily coding problem/problem_no_3.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+def Serialize(root,arr):
+    if root is None:
+        arr.append(-1)
+        return
+    else:
+        arr.append(root.data)
+        Serialize(root.left)
+        Serialize(root.right)
+        
+def Deserialize(arr):
+    index = 0
+    if index == len(arr):
+        return None
+    val = arr[index]
+    index += 1
+    if arr[index] == -1:
+        return None
+    node = Node(val)
+    node.left = Deserialize(arr)
+    node.right = Deserialize(arr)
+    return node
+    
+    
